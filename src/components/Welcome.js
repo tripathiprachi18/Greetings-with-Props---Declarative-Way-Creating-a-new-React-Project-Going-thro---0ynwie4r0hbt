ee3eb2a11e60f3cf673dae9947d15a4a09741abe
Prachi Tripathi <114851153+tripathiprachi18@users.noreply.github.com>
2023-01-18 15:30:25 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react';
+
+function Welcome({name}){
+  return(
+    <div>
+    <h1>` Hello ${name}`</h1>
+    <h2>Welcome to the coding society</h2>
+    </div>
+    );
+  
+};
+export default Welcome;
