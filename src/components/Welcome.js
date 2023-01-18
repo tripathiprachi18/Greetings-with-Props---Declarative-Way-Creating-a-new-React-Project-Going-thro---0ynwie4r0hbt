eaa9c66f3a6c77a8d30c1ed1876c6012483180f5
Prachi Tripathi <114851153+tripathiprachi18@users.noreply.github.com>
2023-01-18 15:38:53 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,8 @@ import React from 'react';
 function Welcome({name}){
   return(
     <div>
-    <h1>` Hello ${name}`</h1>
-    <h2>Welcome to the coding society</h2>
+    <h1>{ `Hello ${name}`}</h1>
+    <h2>{Welcome to the coding society}</h2>
     </div>
     );
   
