a7ceaac50a2d3be9b0de0b2a218c535569f6b1d0
Prachi Tripathi <114851153+tripathiprachi18@users.noreply.github.com>
2023-04-04 00:42:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
- const[name,Setname]=useState("Prachi");
- const [click,setClick]=useState(true);
- const handleClick =(e)=>{
-  Setname(e.target.value)
-  setClick(!click)
- }
+
+
   return (
-   {click ?<input type='text'placeholder='Enter Name' value={name} onchange={handleClick} /> : <Welcome />}
+    <Welcome name="Prachi" />
   )
 }
 
